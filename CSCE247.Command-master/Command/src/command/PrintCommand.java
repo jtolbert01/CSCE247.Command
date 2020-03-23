fee799855a0e751f53694d9922c83573f450d65e
jake5soccer <43284849+jake5soccer@users.noreply.github.com>
2020-03-23 19:16:48 -0400

    Add files via upload

--- CSCE247.Command-master/Command/src/command/PrintCommand.java
+++ CSCE247.Command-master/Command/src/command/PrintCommand.java
@@ -1,10 +1,16 @@
-/*
- * Joshua S Tolbert
- */
-
 package command;
 
 
-public class PrintCommand {
-
+public class PrintCommand implements Command {
+    
+    private Document document;
+    
+    public PrintCommand(Document doc) {
+        this.document = doc;
+    }
+    
+    @Override
+    public void execute() {
+        this.document.print();
+    }
 }
--- CSCE247.Command-master/Command/src/command/SaveCommand.java
+++ CSCE247.Command-master/Command/src/command/SaveCommand.java
@@ -1,10 +1,16 @@
-/*
- * Joshua S Tolbert
- */
-
 package command;
 
 
-public class SaveCommand {
-
+public class SaveCommand implements Command {
+    
+    private Document document;
+    
+    public SaveCommand(Document doc) {
+        this.document = doc;
+    }
+    
+    @Override
+    public void execute() {
+        this.document.save();
+    }
 }
